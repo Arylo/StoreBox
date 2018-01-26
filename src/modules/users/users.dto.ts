3c1a76ed3f6c71e91f1e3c300ac7c18e4ccb7aa2
Arylo <arylo.open@gmail.com>
2018-01-26 18:28:41 +0800

    Add Tokens Api

--- src/modules/users/users.dto.ts
+++ src/modules/users/users.dto.ts
@@ -1,6 +1,7 @@
 import { IsString, IsMongoId } from "class-validator";
 import { ObjectId } from "@models/common";
 import { ApiUseTags, ApiModelProperty } from "@nestjs/swagger";
+import { IUidDto } from "@dtos/ids";
 
 export class CreateUserDto {
     @ApiModelProperty({ type: String, description: "Username" })
@@ -18,4 +19,13 @@ export class ModifyPasswordDto {
     @ApiModelProperty({ type: String, description: "New Password" })
     @IsString()
     public readonly newPassword: string;
+}
+
+export class UserTokenParamDto implements IUidDto {
+    @ApiModelProperty({ type: String, description: "User ID" })
+    @IsMongoId()
+    public readonly uid: ObjectId;
+    @ApiModelProperty({ type: String, description: "Token ID" })
+    @IsMongoId()
+    public readonly tid: ObjectId;
 }
