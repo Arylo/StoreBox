6d318152b7d3220f1e4179054af164d6700b7168
Arylo <arylo.open@gmail.com>
2018-02-08 12:52:48 +0800

    Clear Api 304 Statuscode

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,9 +1,16 @@
-import { Module } from "@nestjs/common";
+import { Module, MiddlewaresConsumer } from "@nestjs/common";
 // Modules
 import { DatabaseModule } from "./database/database.module";
-import { ControllersModule } from "./controllers.module";
+import {controllers, ControllersModule} from "./controllers.module";
+import { Clear304Middleware } from "./common/middlewares/clear304.middleware";
 
 @Module({
     modules: [ DatabaseModule, ControllersModule ]
 })
-export class ApplicationModule { }
+export class ApplicationModule {
+    public configure(consumer: MiddlewaresConsumer) {
+        consumer
+            .apply(Clear304Middleware)
+            .forRoutes(...controllers);
+    }
+}
